9fc26ee2d47551de61d251093a6cf416b9ee5ed5
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2021-01-14 17:11:52 +0900

    User수정

--- src/main/java/project/rasp/model/User.java
+++ src/main/java/project/rasp/model/User.java
@@ -1,29 +1,37 @@
 package project.rasp.model;
 
 public class User {
-	private int id;
-	private String name;
-	private String password;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	private int customer_id; // auto_increment로 걸리는 유저 번호임 
+	// (계정 아이디 아님)
+	private String userid ; // 계정 아이디 임
+	private String userpassword ;
+	private String username;
+	// 시간은 나중에 ㅎㅎ
 	
-	
-	
+	public int getCustomer_id() {
+		return customer_id;
+	}
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUserpassword() {
+		return userpassword;
+	}
+	public void setUserpassword(String userpassword) {
+		this.userpassword = userpassword;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 	
 }

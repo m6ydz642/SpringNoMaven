e9214edb76d276288f32ea376a94ebba367b9291
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2021-01-19 17:11:27 +0900

    날짜, 게터 세터 수정

--- src/project/rasp/model/Comment.java
+++ src/project/rasp/model/Comment.java
@@ -1,15 +1,45 @@
 package project.rasp.model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.sun.jmx.snmp.Timestamp;
+
 public class Comment {
 
 	private int board_id;
 	private int comment_number;
+	private String userid;
 	private String username;
 	private String comment_content;
-	// 날짜 나중에 ㅎㅎ
+	private String[] write_date;
+	
+	
+	
+
+
+	public String[] getWrite_date() {
+		return write_date;
+	}
+	public void setWrite_date(String write_date) throws ParseException {
+		
+		/*게터에서 애초부터 년,월,일로  잘라서 나가게 해놈 
+		 * jstl태그로 안됨 ㅡ.ㅡ ;;; 
+		 * 
+		 * */
+		String[] split = write_date.split(" ");
+		this.write_date = split;
+	}
 	
 	public int getBoard_id() {
 		return board_id;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
 	}
 	public void setBoard_id(int board_id) {
 		this.board_id = board_id;

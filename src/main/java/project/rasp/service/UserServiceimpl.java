def1da40b57122b4be6a6bcd0f047d099fb1a369
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2021-01-21 17:09:52 +0900

    Update UserServiceimpl.java

--- src/main/java/project/rasp/service/UserServiceimpl.java
+++ src/main/java/project/rasp/service/UserServiceimpl.java
@@ -1,10 +1,12 @@
 package project.rasp.service;
+import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import project.rasp.mapper.UserMapper;
 import project.rasp.mapper.Userimpl;
 import project.rasp.model.User;
 
@@ -20,17 +22,17 @@ public class UserServiceimpl implements UserService {
 	public boolean UserLoginCheck(User user, HttpSession session) {
 		System.out.println("UserLoginCheck 서비스 호출 : " + user);
 		
-		boolean result = userimpl.UserLoginCheck(user, session);
+		boolean result = userimpl.UserLoginCheck(user);
 		if (result) {
-		System.out.println("서비스 호출 정보 : " + user.getUserid());
-		System.out.println("서비스 호출정보 2 : " + user.getUserpassword());
+		System.out.println("로그인 체크 호출 유저아이디 : " + user.getUserid());
+		System.out.println("로그인 체크 호출 유저패스워드 :  " + user.getUserpassword());
+		System.out.println("로그인 체크 호출 유저이름 : " + user.getUsername());
 		
+		}else {
+			System.out.println("로그인 체크 실패 result 반환 : " + result);
 		}
 		
 		return result;
 	}
-
-
-
 
 }
